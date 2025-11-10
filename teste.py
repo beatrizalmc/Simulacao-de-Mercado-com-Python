636f8ed3239c2342a430a9328f090b029e53fc33
Beatriz Almeida <beatriz_almc@hotmail.com>
2025-11-10 15:13:58 -0300

    Projeto finalizado - Simulação de Mercado com Python

---
+++ teste.py
@@ -0,0 +1,9 @@
+from models.produto import Produto
+
+ps4 = Produto('Playstation 4', 1789.44)
+xbox = Produto('Xbox 360', 1699.00)
+
+print(ps4)
+print(xbox)
+
+#só para teste.
---
+++ utils/helper.py
@@ -0,0 +1,2 @@
+def formata_float_str_moeda (valor: float) -> str: #transforma um float em str moeda
+    return print(f'R$ {valor:,.2f}')
